27e63ffd36d6c24cdab82bfc03cac25a65c5ff2e
Regis Millet <regmillet@ct2c.fr>
2017-03-12 19:01:45 +0100

    'Simple' express server

--- server.js
+++ server.js
@@ -1,23 +1,79 @@
-let http = require('http'),
-    fs = require('fs');
+'use strict'
+// Set up ======================================================================
+let http            = require('http')
+let express         = require('express')
+let bodyParser      = require('body-parser')
+let cookieParser    = require('cookie-parser')
+let favicon         = require('serve-favicon')
+let methodOverride  = require('method-override')
+let logger          = require('morgan')
 
-fs.readFile('./public/index.html', function (err, html) {
-  if (err) {
-    throw err;
-  }
+let app             = express()
 
-  let server = http.createServer(function(request, response){
-    response.writeHead(200);
-    response.write(html);
-    
-    response.end();
-  })
+let router          = express.Router()
 
-  server.listen(8080);
-  console.log('Addresse: http://localhost:8080')
+// Set a static folder used by express. This folder contains our Angular application
+// Take a look at: expressjs.com/en/starter/static-files.html
+console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+
+// Set logs
+app.use(logger('combined'));
+
+// https://www.npmjs.com/package/serve-favicon
+// Uncomment after placing your favicon in /public:
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+
+// Set parser to get the body data request
+// ref: https://www.npmjs.com/package/body-parser
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
+
+// Set parser for cookies, same logic as bodyParser but for cookies
+// Has some extra options, take a look at: https://www.npmjs.com/package/cookie-parser
+app.use(cookieParser())
+
+
+// Override HTTP methods to support DELETE PUT, if client device doesn't support them
+app.use(methodOverride('X-HTTP-Method-Override'))
+
+// Catch 404 not found errors and forward to error handler
+app.use((request, response, next) => {
+  let err = new Error('Not Found')
+  console.log("Were here")
+  err.status = 404
+  next(err)
 })
 
-fs.writeFile('./test.txt', '{test: "My test"}', function(err, file){
-  console.log(err);
-  console.log(file);
+// To better understand middlewares: 
+// http://expressjs.com/en/guide/writing-middleware.html
+// Middleware to catch all errors
+app.use((error, request, response, next) => {
+  console.error(error.stack)
+  console.log("Were also here")
+  response.status(error.status || 500).send(error.message)
 })
+
+//Export function startServer with port, path and callback params it's used by brunch
+// brunch? -> https://www.npmjs.com/package/brunch
+// Brunch is not used here but it is good to know it
+exports.startServer = (port, path, callback) => {
+    // Create server
+    let server = http.Server(app);
+    // Listening
+    port = process.env.PORT || port
+    server.listen(port, callback)
+    console.log(`server listening on port ${port}`)
+
+    //Intercept when application killed
+    process.on('SIGINT', function() {
+        console.log("\nStopping...")
+        process.exit()
+    });
+}
--- wild-blog-app.js
+++ wild-blog-app.js
@@ -2,7 +2,7 @@
 This file is the entry point
 */
 'use strict'
-let port = process.env.PORT || 8000
+let port = process.env.PORT || 8080
 let server = require('./server.js')
 
-
+server.startServer(port)
