cc0c1f804608e925733ef0b5cf6d31f65fc619f9
Regis Millet <regmillet@ct2c.fr>
2017-03-12 19:27:40 +0100

    Angular with webpack and babel

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,10 @@
 This file is the entry point of Angular application.
 There are all depedencies (module)
 */
+
+import angular from 'angular'
+import angularUIRouter from 'angular-ui-router'
+
+angular.module('app', [
+    angularUIRouter
+])
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+    entry: {
+        javascript: './public/js/app.js'
+    },
+    output: {
+        filename: './public/js/app.min.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /(node_modules|bower_components)/,
+            query: {
+                presets: ['env']
+            }
+        }]
+    }
+}
