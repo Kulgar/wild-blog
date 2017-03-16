eeb1a5bc10f7a89a5bd6b1b0862a43c16e3160cb
Regis Millet <regmillet@ct2c.fr>
2017-03-16 18:44:08 +0100

    First we create User model and schema with JWT and password encrypt methods

--- config/env.js
+++ config/env.js
@@ -1,10 +1,10 @@
 module.exports = {
     "development": {
         db: process.env.MONGODB_URI ||  'mongodb://localhost:27017/blog',
-        token : process.env.SECRET_TOKEN || 'secretToken'
+        secretToken: process.env.SECRET_TOKEN || 'secretToken'
     },
     "production": {
         db: process.env.MONGODB_URI ||  'mongodb://localhost:27017/blog',
-        token : process.env.SECRET_TOKEN || 'secretToken'
+        secretToken: process.env.SECRET_TOKEN || 'secretToken'
     }
 }
